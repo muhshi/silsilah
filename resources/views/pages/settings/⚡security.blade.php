09c6ad5aed0a6cf02c7b1dda239ae5556aa8ff7d
muhshi <amuhshi@gmail.com>
2026-07-17 22:09:38 +0700

    UI Polish: Remove dark mode setting from profile, translate settings layout, profile, and security pages to Indonesian

--- resources/views/pages/settings/⚡security.blade.php
+++ resources/views/pages/settings/⚡security.blade.php
@@ -10,7 +10,7 @@ use Livewire\Attributes\On;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
-new #[Title('Security settings')] class extends Component {
+new #[Title('Pengaturan Keamanan')] class extends Component {
     use PasswordValidationRules;
 
     public string $current_password = '';
@@ -88,13 +88,13 @@ new #[Title('Security settings')] class extends Component {
 <section class="w-full">
     @include('partials.settings-heading')
 
-    <flux:heading class="sr-only">{{ __('Security settings') }}</flux:heading>
+    <flux:heading class="sr-only">{{ __('Pengaturan Keamanan') }}</flux:heading>
 
-    <x-pages::settings.layout :heading="__('Update password')" :subheading="__('Ensure your account is using a long, random password to stay secure')">
+    <x-pages::settings.layout :heading="__('Perbarui sandi')" :subheading="__('Pastikan akun Anda menggunakan kata sandi yang panjang dan acak agar tetap aman')">
         <form method="POST" wire:submit="updatePassword" class="mt-6 space-y-6">
             <flux:input
                 wire:model="current_password"
-                :label="__('Current password')"
+                :label="__('Kata sandi saat ini')"
                 type="password"
                 required
                 autocomplete="current-password"
@@ -102,7 +102,7 @@ new #[Title('Security settings')] class extends Component {
             />
             <flux:input
                 wire:model="password"
-                :label="__('New password')"
+                :label="__('Kata sandi baru')"
                 type="password"
                 required
                 autocomplete="new-password"
@@ -110,7 +110,7 @@ new #[Title('Security settings')] class extends Component {
             />
             <flux:input
                 wire:model="password_confirmation"
-                :label="__('Confirm password')"
+                :label="__('Konfirmasi sandi')"
                 type="password"
                 required
                 autocomplete="new-password"
@@ -120,26 +120,26 @@ new #[Title('Security settings')] class extends Component {
             <div class="flex items-center gap-4">
                 <div class="flex items-center justify-end">
                     <flux:button variant="primary" type="submit" class="w-full" data-test="update-password-button">
-                        {{ __('Save') }}
+                        {{ __('Simpan') }}
                     </flux:button>
                 </div>
 
                 <x-action-message class="me-3" on="password-updated">
-                    {{ __('Saved.') }}
+                    {{ __('Tersimpan.') }}
                 </x-action-message>
             </div>
         </form>
 
         @if ($canManageTwoFactor)
             <section class="mt-12">
-                <flux:heading>{{ __('Two-factor authentication') }}</flux:heading>
-                <flux:subheading>{{ __('Manage your two-factor authentication settings') }}</flux:subheading>
+                <flux:heading>{{ __('Autentikasi Dua Faktor (2FA)') }}</flux:heading>
+                <flux:subheading>{{ __('Kelola pengaturan autentikasi dua faktor Anda') }}</flux:subheading>
 
                 <div class="flex flex-col w-full mx-auto space-y-6 text-sm" wire:cloak>
                     @if ($twoFactorEnabled)
                         <div class="space-y-4">
                             <flux:text>
-                                {{ __('You will be prompted for a secure, random pin during login, which you can retrieve from the TOTP-supported application on your phone.') }}
+                                {{ __('Anda akan diminta memasukkan PIN acak yang aman saat login, yang dapat Anda ambil dari aplikasi TOTP di ponsel Anda.') }}
                             </flux:text>
 
                             <div class="flex justify-start">
@@ -147,7 +147,7 @@ new #[Title('Security settings')] class extends Component {
                                     variant="danger"
                                     wire:click="disable"
                                 >
-                                    {{ __('Disable 2FA') }}
+                                    {{ __('Matikan 2FA') }}
                                 </flux:button>
                             </div>
 
@@ -156,7 +156,7 @@ new #[Title('Security settings')] class extends Component {
                     @else
                         <div class="space-y-4">
                             <flux:text variant="subtle">
-                                {{ __('When you enable two-factor authentication, you will be prompted for a secure pin during login. This pin can be retrieved from a TOTP-supported application on your phone.') }}
+                                {{ __('Saat Anda mengaktifkan 2FA, Anda akan diminta memasukkan PIN aman saat login. PIN ini dapat diambil dari aplikasi TOTP di ponsel Anda.') }}
                             </flux:text>
 
                             <flux:modal.trigger name="two-factor-setup-modal">
@@ -164,7 +164,7 @@ new #[Title('Security settings')] class extends Component {
                                     variant="primary"
                                     wire:click="$dispatch('start-two-factor-setup')"
                                 >
-                                    {{ __('Enable 2FA') }}
+                                    {{ __('Aktifkan 2FA') }}
                                 </flux:button>
                             </flux:modal.trigger>
 
