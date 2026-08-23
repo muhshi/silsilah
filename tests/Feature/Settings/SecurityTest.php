2cdb7cf41f8f5e22c375b57e4dcfc019f8df4519
muhshi <amuhshi@gmail.com>
2026-08-23 20:49:28 +0700

    test: update SecurityTest assertions for Indonesian localization

--- tests/Feature/Settings/SecurityTest.php
+++ tests/Feature/Settings/SecurityTest.php
@@ -21,8 +21,8 @@ test('security settings page can be rendered', function () {
         ->withSession(['auth.password_confirmed_at' => time()])
         ->get(route('security.edit'))
         ->assertOk()
-        ->assertSee('Two-factor authentication')
-        ->assertSee('Enable 2FA');
+        ->assertSee('Autentikasi Dua Faktor')
+        ->assertSee('Aktifkan 2FA');
 });
 
 test('security settings page requires password confirmation when enabled', function () {
@@ -43,8 +43,8 @@ test('security settings page renders without two factor when feature is disabled
         ->withSession(['auth.password_confirmed_at' => time()])
         ->get(route('security.edit'))
         ->assertOk()
-        ->assertSee('Update password')
-        ->assertDontSee('Two-factor authentication');
+        ->assertSee('Perbarui sandi')
+        ->assertDontSee('Autentikasi Dua Faktor');
 });
 
 test('two factor authentication disabled when confirmation abandoned between requests', function () {
